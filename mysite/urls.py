7e53f8ff671a9ae3945f60e550a414b417ede5ac
lazyass1505 <harshbparmar.cse18@itbhu.ac.in>
2020-06-05 17:38:52 +0530

    Please suggest edits!

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from mysite.core import views as core_views
+
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+]
